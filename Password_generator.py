6a91f0255414cc3511415b59f0e685528a62f914
gywn <63680706+gywn9081@users.noreply.github.com>
2021-02-16 13:42:57 -0600

    finishing letter function
    The function to chose letters is finished and should no longer be considered for combination.

--- Password_generator.py
+++ Password_generator.py
@@ -21,6 +21,9 @@ def number_generator():
 
 # * This function should get the x param from number_generator for space left
 def character_generator(i, x):
+    #! I could not find a better way of defineing my vars before reference
+    has1, has0, acceptable = False, False, False 
+    choice = []
     counter = 0
     characters = []
     lowercase = string.ascii_lowercase
@@ -30,12 +33,29 @@ def character_generator(i, x):
         num_letters = x - 1
         num_letters = int(num_letters / 2)
     characters_remaining = int(x - num_letters)
-    while counter < 2:
-        characters.append(secrets.randbelow(2))
+    while counter < num_letters:
+        choice.append(secrets.randbelow(2))
         counter += 1
-    print(characters)
-
-
+    for y in choice:
+        if y == 1:
+            has1 = True
+        elif y == 0:
+            has0 = True
+        if has1 == True and has0 == True:
+            acceptable = True
+    if acceptable == False:
+        if has1 == False:
+            choice.pop()
+            choice.append(1)
+        elif has0 == False:
+            choice.pop()
+            choice.append(0)
+    for z in choice:
+        if z == 1:
+            characters.append(secrets.choice(uppercase))
+        if z == 0:
+            characters.append(secrets.choice(lowercase))
+    return (characters, characters_remaining)
 
 print(number_generator())
 print(character_generator(0, 6))
